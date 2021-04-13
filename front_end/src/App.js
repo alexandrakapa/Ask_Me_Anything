e7ea2913cc4600b5e3648b99599f98699498ab25
alexandrakapa <aleksandrakapa@hotmail.com>
2021-04-13 21:28:30 +0300

    first try

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,16 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import {AccountBox} from "./components/accountBox";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          You can do this!!!
-        </p>
-      </header>
-    </div>
-  );
+  return  <AppContainer>
+    <AccountBox/>
+  </AppContainer>
+
 }
 
 export default App;
