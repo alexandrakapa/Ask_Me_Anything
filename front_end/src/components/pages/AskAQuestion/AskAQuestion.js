5a1d703cda9cbebdcaf44a622d1a03fead4346d8
alexandrakapa <aleksandrakapa@hotmail.com>
2021-04-20 17:23:24 +0300

    ask a question front done

--- front_end/src/components/pages/AskAQuestion/AskAQuestion.js
+++ front_end/src/components/pages/AskAQuestion/AskAQuestion.js
@@ -1,15 +1,113 @@
 import React from 'react'
 import {AskAQuestionStyle} from "../AskAQuestion/AskAQuestionStyle";
-import {Input} from "../accountBox/common";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+import { withRouter } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+// const AskAQuestion = () => {
+//     // return (
+//     //     <AskAQuestionStyle>
+//     //         <div className="container">
+//     //
+//     //         <h1>Ask a question</h1>
+//     //             <Input type="email" placeholder="User name" />
+//     //     </div>
+//     //         </AskAQuestionStyle>
+//     // )
+//     const { register, handleSubmit } = useForm();
+//
+//
+//
+//     return (
+//         <AskAQuestionStyle>
+//         <h1>Ask a question</h1>
+//             <input type="text" ref={register} name="firstName" />
+//          </AskAQuestionStyle>
+//     )
+// }
+//  export default AskAQuestion;
+
+// function AskAQuestion(props) {
+//
+//     const { register, handleSubmit } = useForm();
+//     const onFormSubmit  = data => console.log(data);
+//
+//     const onErrors = errors => console.error(errors);
+//
+//
+//     return (
+//         <AskAQuestionStyle>
+//         <h1>Ask a question</h1>
+//
+//
+//                 <form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+//                     <input type="text" {...register('firstName')} name="firstName" />
+//                 </form>
+//          </AskAQuestionStyle>
+//     )
+// }
+//  // export default AskAQuestion;
+//
+//
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<AskAQuestion />, rootElement);
+//
+// export default withRouter(AskAQuestion)
+
 const AskAQuestion = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const handleRegistration = (data) => console.log(data);
+    const handleError = (errors) => {};
+    const registerOptions = {
+        name: { required: "Name is required" },
+        email: { required: "Email is required" },
+        password: {
+            required: "Password is required",
+            minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters"
+            }
+        }
+    };
     return (
         <AskAQuestionStyle>
-            <div className="container">
-
             <h1>Ask a question</h1>
-                <Input type="email" placeholder="User name" />
-        </div>
-            </AskAQuestionStyle>
-    )
-}
+        <Form onSubmit={handleSubmit(handleRegistration, handleError)}>
+            <FormGroup>
+                <Label>Question title</Label>
+                <Input name="title" {...register('title')} />
+                <small className="text-danger">
+                    {/*{errors.name && errors.name.message}*/}
+                </small>
+            </FormGroup>
+            <FormGroup>
+                <Label>Question text</Label>
+                <Input
+                    type="textarea"
+                    name="text"
+                    {...register('text')}
+                />
+                <small className="text-danger">
+                    {/*{errors.email && errors.email.message}*/}
+                </small>
+            </FormGroup>
+            <FormGroup>
+                <Label>Keywords</Label>
+                <Input
+                    name="keywords"
+                    {...register('keywords')}
+                />
+                <small className="text-danger">
+                    {/*{errors.password && errors.password.message}*/}
+                </small>
+            </FormGroup>
+            <div>
+            <Button type="submit">Submit</Button>
+            <Button type="cancel">Cancel</Button>
+            </div>
+        </Form>
+        </AskAQuestionStyle>
+    );
+};
 export default AskAQuestion;
--- front_end/src/components/pages/AskAQuestion/AskAQuestionStyle.js
+++ front_end/src/components/pages/AskAQuestion/AskAQuestionStyle.js
@@ -4,12 +4,11 @@ export const AskAQuestionStyle = styled.div`
   h1 {
     line-height: 2;
     display: flex;
-    margin-bottom: 10%;
-    margin-top: 5%;
+    margin-top: 3%;
     color: #000000;
     font-size: 30px;
     font-weight: 200;
-    margin-left: -10%;
+    margin-left: 10%;
   }
 
   h3 {
@@ -19,32 +18,157 @@ export const AskAQuestionStyle = styled.div`
   p {
     font-size: 15px;
   }
-
-`
-
-export const Input = styled . input `  
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgb(121, 63, 212);
-  padding: 0px 10px;
-  //border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-  margin-bottom: 7%;
-  margin-left: -15%;
   
 
-  &::placeholder {
-    color: rgb(0, 0, 0);
+p {
+  color: #bf1650;
+}
+
+p::before {
+  display: inline;
+  content: "⚠ ";
+}
+
+Form {
+width: 70%;
+margin-left:10%;
+margin-top: 1%;
+height: 80%;
+}
+
+FormGroup {
+width: 100%;
+margin-left:10%;
+margin-top: 5%;
+}
+
+
+Input{
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  height: 80%;
+  border-radius: 4px;
+  border: 2px solid #474747;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+Input:focus { 
+    outline: none !important;
+    border: 2px solid #8d17cd;
+    box-shadow: 0 0 10px #000000;
+}
+textarea:focus { 
+    outline: none !important;
+    border: 2px solid #8d17cd;
+    box-shadow: 0 0 10px #000000;;
+}
+
+
+ textarea {
+  display: block;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 2px solid #474747;
+  padding: 10px 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
   }
 
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgb(121, 63, 212);
-  }
+Label {
+  line-height: 2;
+  text-align: left;
+  display: block;
+  margin-bottom: 13px;
+  margin-top: 20px;
+  color: #000000;
+  font-size: 16px;
+  font-weight: 200;
+}
 
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(95, 91, 91);
-  }
+button[type="submit"],
+input[type="submit"] {
+  background: rgb(74, 20, 156);
+  color: white;
+  text-transform: uppercase;
+  border: none;
+  margin-top: 3%;
+  padding: 1.5%;
+  font-size: 14px;
+  font-weight: 100;
+  letter-spacing: 10px;
+  display: inline-block;
+  margin-right: 5%;
+}
+
+button[type="cancel"],
+input[type="cancel"] {
+  background: rgb(74, 20, 156);
+  color: white;
+  text-transform: uppercase;
+  border: none;
+  margin-top: 3%;
+  padding: 1.5%;
+  font-size: 14px;
+  font-weight: 100;
+  letter-spacing: 10px;
+  display: inline-block;
+}
+
+button[type="submit"]:hover,
+input[type="submit"]:hover,
+button[type="cancel"]:hover,
+input[type="cancel"]:hover{
+  filter: brightness(1);
+  background: rgb(140, 91, 255);
+}
+
+input:disabled {
+  opacity: 0.4;
+}
+
+input[type="button"]:hover {
+  transition: 0.3s all;
+}
+
+button[type="submit"],
+input[type="button"],
+input[type="submit"] {
+  -webkit-appearance: none;
+}
+
+
+button[type="button"] {
+  display: block;
+  appearance: none;
+  background: #333;
+  color: #ffffff;
+  border: none;
+  text-transform: uppercase;
+  padding: 10px 20px;
+  border-radius: 4px;
+}
+
+hr {
+  margin-top: 30px;
+}
+
+button {
+  display: block;
+  appearance: none;
+  margin-top: 40px;
+  border: 1px solid #333;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+  padding: 10px 20px;
+  border-radius: 4px;
+}
+
+
    `;
