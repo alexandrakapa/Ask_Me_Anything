fccc82b8d47fbc63383d89c04050a93e3c729cc6
alexandrakapa <aleksandrakapa@hotmail.com>
2021-04-22 22:54:07 +0300

    authentication in front done

--- front_end/src/components/pages/accountBox/loginForm.jsx
+++ front_end/src/components/pages/accountBox/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import {useState} from 'react';
 import {
     BoldLink,
     BoxContainer,
@@ -10,40 +11,88 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import { useHistory } from "react-router";
 
+ export function LoginForm(props) {
 
-export function LoginForm(props) {
-    const { switchToSignup } = useContext(AccountContext);
-    function myFunction() {
+     const emailRef = React.useRef();
+     const passwordRef = React.useRef();
+     const { switchToSignup } = useContext(AccountContext);
+     const history = useHistory();
+
+    function onSubmit() {
         alert("The form was submitted");
+        // console.log(emailRef.current.value)
+        // console.log(passwordRef.current.value)
+
+        let empInfo={
+            username:emailRef.current.value,
+            password:passwordRef.current.value
+        };
+        console.log(empInfo);
+        const getdata= JSON.stringify(empInfo);
+        console.log(getdata);
+
+
+        const fetch = require('node-fetch');
+
+        fetch('http://localhost:3000/signin',{
+            method: 'POST',
+            body: JSON.stringify(empInfo),
+            headers:{'Content-type':'application/json'}
+        }).then(res => res.json() )
+            .then( json => {
+                        // this.props.setUserData(json.accessToken, json.username);
+                if(json.token!=="" ) {
+                    localStorage.setItem('token', json.token);
+                    history.push({
+                        pathname:  "/ask_a_question"
+                    });
+                }
+                localStorage.setItem('token', json.token);
+                const tok = localStorage.getItem('token');
+                console.log("here")
+                console.log(tok);
+
+
+                // console.log(json.token)
+                }
+            )
+            .catch(err => console.log(err));
     }
 
 
+        return (
+            <BoxContainer>
+                <FormContainer>
+                        <Input
+                            name="email"
+                            ref={emailRef}
+                            type="email"
+                            placeholder="User name"
+                        />
+                        <Input
+                            name="password"
+                            ref={passwordRef}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </FormContainer>
+                    <Marginer direction="vertical" margin={10}/>
+                    <MutedLink href="#">Forgot your password?</MutedLink>
+                    <Marginer direction="vertical" margin="1.6em"/>
+                    <SubmitButton type="submit" onClick={onSubmit}>Login</SubmitButton>
+                    <Marginer direction="vertical" margin="1em"/>
+                    <MutedLink href="#">
+                        Don't have an account?{" "}
+                        <BoldLink href="#" onClick={switchToSignup}>
+                        Signup
+                    </BoldLink>
+                </MutedLink>
+            </BoxContainer>
 
-    return (
-        <BoxContainer>
-            <FormContainer>
-                <Input
-                    type="email"
-                    placeholder="User name"
-                />
-                <Input
-                    type="password"
-                    placeholder="Password"
-                />
-            </FormContainer>
-            <Marginer direction="vertical" margin={10} />
-            <MutedLink href="#">Forgot your password?</MutedLink>
-            <Marginer direction="vertical" margin="1.6em" />
-            <SubmitButton type="submit" onsubmit="myFunction()">Login</SubmitButton>
-            <Marginer direction="vertical" margin="1em" />
-            <MutedLink href="#">
-                Don't have an account?{" "}
-                <BoldLink href="#" onClick={switchToSignup}>
-                    Signup
-                </BoldLink>
-            </MutedLink>
-        </BoxContainer>
+        );
 
-    );
 }
+
+
