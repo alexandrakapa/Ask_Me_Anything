4e20b869ea6235269da3a1e955ccee7aa0e5a326
Giorgoulakis Nikos <nikos@Giorgoulakiss-MacBook-Air.local>
2021-06-01 18:41:31 +0300

    small changes in addresses

--- micro-question/ormconfig.ts
+++ micro-question/ormconfig.ts
@@ -4,9 +4,9 @@ export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   port: 5432,
   host: '127.0.0.1',
-  database: 'question_microservice',
-  username: "postgres",
-  password: "fresdr7l389",
+  database: 'micro_question_db',
+  username: "qna_admin",
+  password: "1999",
   synchronize: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
 }
